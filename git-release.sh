12fe69a44bb138089cd5e26881f129388feb8535
ChethiyaNishanath <chethiyanishanath@gmail.com>
2025-12-29 13:54:39 +0530

    Git release script

---
+++ git-release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VERSION="${1:-v1.0.0}"
+
+echo "Checking out master branch"
+git checkout master
+
+echo "Creating tag: $VERSION"
+git tag -a "$VERSION" -m "Release $VERSION"
+
+echo "Pushing tag to remote"
+git push origin "$VERSION"
+
+echo "Running ECR push script"
+
+./push-to-ecr.sh
